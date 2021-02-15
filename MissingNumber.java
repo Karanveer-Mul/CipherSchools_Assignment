6c83e519fa385cd726740d9056eddac92d5f1d65
Karanveer-Mul <karanveer7171@gmail.com>
2021-02-16 02:07:36 +0530

    Missing number in range

---
+++ MissingNumber.java
@@ -0,0 +1,15 @@
+public class MissingNumber {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 4, 6, 3, 7, 8};
+
+        int sum = 0;
+        int sumArr = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            sumArr += arr[i];
+            sum += i+1;
+        }
+
+        System.out.println(sum + arr.length + 1 - sumArr);
+    }
+}
